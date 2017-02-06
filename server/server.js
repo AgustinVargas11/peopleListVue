a9027db834e2cc9ae28e3796a4d6f866220d316c
Agustin Vargas <agustinvargas11@gmail.com>
2017-02-06 10:03:07 -0500

    remove cdn, add dependency add back file removed in error

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public/')));
+app.use('/css', express.static(path.join(__dirname, '..', 'node_modules/bulma/css')));
 
 app.use('/api/person', personRouter);
 
